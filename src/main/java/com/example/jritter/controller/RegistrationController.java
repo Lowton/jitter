198df6b6f0f3033224f1b8389811a279fc090cf9
Lowton <iskeyiv@gmail.com>
2019-01-29 23:35:08 +0400

    Create security config and login logic Refactor controllers

---
+++ src/main/java/com/example/jritter/controller/RegistrationController.java
@@ -0,0 +1,38 @@
+package com.example.jritter.controller;
+
+import com.example.jritter.domain.Role;
+import com.example.jritter.domain.User;
+import com.example.jritter.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model){
+        User userFromDB = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDB != null){
+            model.put("message", "Пользователь существует!");
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/jritter/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.jritter.domain;
+
+public enum Role {
+    USER;
+}
